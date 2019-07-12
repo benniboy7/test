6d6b58abbd1ab795cedb0c0c25777435bd3df859
benniboy7 <ben@sunsetais.com>
2019-07-13 03:34:22 +0500

    second

--- src/components/ModalContainer/index.jsx
+++ src/components/ModalContainer/index.jsx
@@ -43,7 +43,20 @@ export default class ModalContainer extends React.Component {
                   <CloseButton onClick={onClose}>
                     <CloseIconStyled />
                   </CloseButton>
-                  {children}
+                    <form name="contact" method="post">
+                              <p>
+                                <label>Your Name: <input type="text" name="name"/></label>
+                              </p>
+                              <p>
+                                <label>Your Email: <input type="email" name="email"/></label>
+                              </p>
+                              <p>
+                                <label>Message: <textarea name="message"></textarea></label>
+                              </p>
+                              <p>
+                                <button type="submit">Send</button>
+                              </p>
+                            </form>
                 </Content>
               </Col>
             </Row>
