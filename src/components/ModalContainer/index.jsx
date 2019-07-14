3e8011d028d52120e99b452b10e96e902cb52405
benniboy7 <ben@sunsetais.com>
2019-07-15 03:27:03 +0500

    thirdlnsssssssssssssfv

--- src/components/ModalContainer/index.jsx
+++ src/components/ModalContainer/index.jsx
@@ -43,9 +43,22 @@ export default class ModalContainer extends React.Component {
                   <CloseButton onClick={onClose}>
                     <CloseIconStyled />
                   </CloseButton>
-                    <form>
-                      <FormInput />
-                    </form>
+                    
+          <form name="contact" method="post">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p>
+                      <label>Your Name: <input type="text" name="name"/></label>
+                    </p>
+                    <p>
+                      <label>Your Email: <input type="email" name="email"/></label>
+                    </p>
+                    <p>
+                      <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                      <button type="submit">Send</button>
+                    </p>
+                  </form>
                 </Content>
               </Col>
             </Row>
--- src/scenes/Index/Contact/index.jsx
+++ src/scenes/Index/Contact/index.jsx
@@ -33,21 +33,6 @@ function Contact({ innerRef = () => {} }) {
             Santa Monica, CA 90401
             </TextContainer>
           </Col>
-          <form name="contact" method="post">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p>
-                      <label>Your Name: <input type="text" name="name"/></label>
-                    </p>
-                    <p>
-                      <label>Your Email: <input type="email" name="email"/></label>
-                    </p>
-                    <p>
-                      <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <p>
-                      <button type="submit">Send</button>
-                    </p>
-                  </form>
         </Row>
       </GridStyled>
     </Container>
