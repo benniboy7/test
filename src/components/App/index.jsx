9b18fb5fccedeef3b5e46ddb673b29482d0ca583
benniboy7 <ben@sunsetais.com>
2019-07-15 02:57:08 +0500

    thirdlnssghgj

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -64,7 +64,7 @@ export default function App() {
           {/*<Switch>*/}
             {/*<Route path={`/:type(${typesRegex})`} component={Type} />*/}
             {/*<Route path="/:scroll(about|services|testimonials|contact)" component={Index} />*/}
-            {/*<Route path="/" component={Index} />*/}
+            <Route path="/" component={Index} />
           {/*</Switch>*/}
           {/*<Footer />*/}
           <MobileQuote />
