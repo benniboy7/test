7be6b07a49526caebbb85fd1e641a825d5c808e7
benniboy7 <ben@sunsetais.com>
2019-07-19 23:44:24 +0500

    video

--- src/components/ModalContainer/icons.jsx
+++ src/components/ModalContainer/icons.jsx
@@ -1,27 +1,67 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
 
-import { toClassNameObj } from 'src/services/helpers';
+import { CloseIcon } from './icons';
 
-function CloseIcon({ className = '' }) {
+function Body({ className }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      {...toClassNameObj(className)}
-    >
-      <path
-        fill="#FFF"
-        fillRule="nonzero"
-        d="M8 6.586L13.657.929a1 1 0 1 1 1.414 1.414L9.414 8l5.657 5.657a1 1 0 1 1-1.414 1.414L8 9.414l-5.657 5.657A1 1 0 1 1 .93 13.657L6.586 8 .929 2.343A1 1 0 1 1 2.343.93L8 6.586z"
-      />
-    </svg>
+    <Helmet>
+      <body className={className} />
+    </Helmet>
   );
 }
-CloseIcon.propTypes = {
-  className: PropTypes.string,
-};
 
-export { CloseIcon };
+export const Container = styled.div`
+  backdrop-filter: blur(1.8px);
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  height: 100%;
+  left: 0;
+  overflow: auto;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 3;
+`;
+
+export const Content = styled.div`
+  position: relative;
+`;
+
+export const CloseIconStyled = styled(CloseIcon)`
+  transition: transform 0.3s ease-in-out;
+`;
+
+export const CloseButton = styled.button.attrs({ type: 'button' })`
+  background: ${props => props.theme.colors.blackThree};
+  border: 0;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.15);
+  height: 50px;
+  padding: 0;
+  position: absolute;
+  right: 0;
+  top: -66px;
+  width: 50px;
+  @media (min-width: ${props => props.theme.flexboxgrid.breakpoints.sm}em) {
+    right: -66px;
+    top: 0;
+  }
+  &:hover ${CloseIconStyled} {
+    transform: rotate(180deg);
+  }
+`;
+
+export const BodyStyled = styled(Body)`
+  overflow: hidden;
+`;
+
+export const Center = styled.div`
+  margin: auto;
+  padding: 5rem 1rem;
+  width: 100%;
+  @media (min-width: ${props => props.theme.flexboxgrid.breakpoints.sm}em) {
+    padding: 0;
+    width: auto;
+  }
+`;
