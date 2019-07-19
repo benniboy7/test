cdff8f64a993d8f2b923c1b1f475ab380c74b2bc
benniboy7 <ben@sunsetais.com>
2019-07-19 23:54:33 +0500

    videomm

--- src/components/ModalContainer/index.jsx
+++ src/components/ModalContainer/index.jsx
@@ -43,7 +43,7 @@ export default class ModalContainer extends React.Component {
                   <CloseButton onClick={onClose}>
                     <CloseIconStyled />
                   </CloseButton>
-         
+                  {children}
                 </Content>
               </Col>
             </Row>
